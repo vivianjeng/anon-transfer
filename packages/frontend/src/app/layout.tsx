387e8936b1278c155e04d61855906be3d848f05f
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-12-15 15:56:28 +0800

    chore: update metadata

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -6,9 +6,51 @@ import { GlobalContextProvider } from '@/contexts/User'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-    title: 'Anon transfer',
+    title: 'Anon Transfer',
     description:
-        'An anonymous transfer system that disconnects all addresses and values.',
+        'Transfer X ETH with A account, withdraw Y ETH with B account.',
+    applicationName: 'Anon transfer',
+    authors: { name: '@vivianjeng', url: 'https://github.com/vivianjeng' },
+    generator: 'Next.js',
+    keywords: [
+        'Anonymous',
+        'Transfer',
+        'ETH',
+        'Ethereum',
+        'ZKP',
+        'Zero Knowledge Proof',
+        'UniRep',
+        'Privacy',
+    ],
+    icons: {
+        icon: 'https://anon-transfer.online/favicon.ico',
+        apple: 'https://anon-transfer.online/apple-touch-icon.png',
+    },
+    manifest: 'https://anon-transfer.online/manifest.json',
+    openGraph: {
+        type: 'website',
+        url: 'https://example.com',
+        title: 'Anon Transfer',
+        description:
+            'Transfer X ETH with A account, withdraw Y ETH with B account.',
+        siteName: 'Anon Transfer',
+        images: [
+            {
+                url: 'https://anon-transfer.online/og.png',
+            },
+        ],
+    },
+    twitter: {
+        card: 'summary_large_image',
+        site: '@UniRep_Protocol',
+        creator: '@vivi4322',
+        images: 'https://anon-transfer.online/og.png',
+    },
+    appleWebApp: {
+        capable: true,
+        title: 'Anon Transfer',
+        statusBarStyle: 'default',
+    },
 }
 
 export default function RootLayout({
